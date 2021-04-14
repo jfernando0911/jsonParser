83c91623b38d9c734f8a8d71711ec8f450a37cf5
jfernando0911 <jfernando0911@gmail.com>
2021-04-14 18:24:55 -0500

    primer commit

--- src/jsonparser/JsonParser.java
+++ src/jsonparser/JsonParser.java
@@ -16,7 +16,7 @@ public class JsonParser {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Empezando el proyecto");
+        System.out.println("Test");
     }
     
 }
